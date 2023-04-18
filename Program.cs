81737a33a40e03647d2cd2bbec11e85dcdca433c
Jennigds <jennifergds89@gmail.com>
2023-04-17 22:02:17 -0300

    Resultado

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿string frasedigitada, fraseDislalia;
+Console.WriteLine("Digite uma flase: ");
+
+frasedigitada = Console.ReadLine()!;
+
+fraseDislalia = frasedigitada
+    .Replace("r", "l")
+    .Replace("R", "L");
+
+
+Console.WriteLine(fraseDislalia);
